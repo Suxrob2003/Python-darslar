451ab46ea0f5b0dabc4a46fcc4bd123f505d696e
suxrob2003 <suxrobshoimov@gmail.com>
2023-03-09 08:58:39 +0300

    deploy

---
+++ dars.py
@@ -0,0 +1,40 @@
+# Shart operatori - 
+
+# Son kiritilganida - agar u musbat bo'lsa, bu son musbat, manfiy bo'lsa manfiy degan javob qaytsin
+
+# Shart operati - qaysidir buyruqni bajarmasa, ayni bitta javobni, boshqa buyruqqa o'tsa, berilgan boshqa buyruqni bajaradi
+
+# Shart :
+# if - agar 
+# elif - mabodo, agar if ishlamasa
+# else
+
+# a = int(input("Son kiriting = "))
+# if a>0:
+#     print("Bu son musbat")
+# elif a<0:
+#     print("Bun son manfiy")
+# a = float(input("sonni tanlang ="))
+# if a>=0:
+#     print("bu son musbat yoki manfiy") 
+# # elif a<0:
+#     # print("bu son manfiy =")
+# else:
+#     print("bu son 0 ga teng ")
+# a= float(input("bu son
+# a =int(input("son kiriting: "))
+# if a<=0:
+#     print(" bu son manfiy")
+# else:
+#     print("bu son no manfiy ,no musbat"
+
+# ishlovchi = ("anvar", "sarvar", "vali")
+# ism = input('Ishlovchiini ismini kiriting: ')
+# if ism in ishlovchi:
+#     print('Bu ishchi bizda ishlaydi)')
+
+# else:
+#     print('Bu odam bizda ishlamaydi!')
+
+
+
---
+++ uy ishi.py
@@ -0,0 +1,47 @@
+# masala 1
+# a= int(input("yoshingizni kiriting = "))
+# d = 2023-a
+# print("tug'ulganyilingiz " , d , "yil")
+# masala 2
+# son1 = int(input("uchburchakning 1- tomonini kiriting ="))
+# son2 = int(input("uchburchakning 2- tomonini kiriting ="))
+# son3 = int(input("uchburchakning 3- tomonini kiriting ="))
+# umumiy =son1+son2+son3
+# print(umumiy)
+# masala 3
+# c = int(input("tomonini kiriting ="))
+# d = int(input("ikkinchi tomonini kiriting ="))
+# a = c-d , c+d
+# print(a)
+# masala 4
+# a = float(input("istalgan sonni kiriting ="))
+# c = a**2 ,a**3
+# print(c)
+# masala 5
+# c = int(input("kvadratning tomonini kiriting = "))
+# a = c**2 , 
+# k = 4*c
+# print("kvadratning yuzi = " , a , "kvadratning primetri = "  ,k  )
+# masala 6
+# c  = float(input("1- sonni kiriting = "))
+# v = float(input("2- sonni kiriting = "))
+# a = c+v , c/v , c*v
+# print(a)
+# masala 7
+# a =int(input(" istalgan sonni kiriting , ildizini chiqaraman ="))
+# c = a**(1/2)
+# print("suragan soningizni ildizi =" ,c)
+# masala 8
+
+# masala 13
+# c = float(input("metr qiiymatni bering  , santmetrga aylantiraman ="))
+# k = c*100
+# print(k)
+# masala 14
+# son = float(input("tonnaning qiymatini kiriting , klogrammga aylantiraman ="))
+# son1 = son*1000
+# print(son1)
+# masala 15
+# son = float(input("sonni kiriting , teskarisini chiqarib beraman ="))
+# son1 = -1*son
+# print(son1)
