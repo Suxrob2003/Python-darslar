ecbda9fa8e8c6ada74987b2857eeaf53d0741def
suxrob2003 <suxrobshoimov@gmail.com>
2023-03-09 10:14:16 +0300

    deploy

---
+++ while.py mavzu.py
@@ -0,0 +1,44 @@
+
+# while takrorlash operatori (while loop)
+
+# son = 1 - sonning boshlanish qismi chegarasi
+# son = 1
+# # while - toki 6gacha ketma ket sanasin, 6 ni o'zi ham kiradi
+# while son<=6:
+#     # print - shu sonni to'liq chop etsin.
+#     print(son, end='')
+#     son +=1
+#     # +=1 - 1tadan qadamlab sanasin
+    
+    
+
+ 
+# print("Son kvadratini chiqarish")
+# savol = "istalgan sonnikiriting "
+# savol +="(dasturni tuxtash uchun 'exit' deb yozing ): "
+# qiymat = ''
+# while qiymat != 'exit':
+#     qiymat = input(savol)
+#     if qiymat != 'exit':
+#         print((qiymat))
+#     else:
+#         break
+   
+   
+
+# print("Son kvadratini chiqarish")
+# savol = "istalgan sonni kiriting "
+# while True:
+#     qiymat = input(savol)
+#     if qiymat == 'x':
+#         break
+#     else:
+#         print(float(qiymat)**2)
+print("tug'ulgan yilingizni chiqarib beraman :")
+son = "yoshingiznikiriting ="
+while True:
+    yoshingiz = input("yoshingizni kiriting:" )
+    if yoshingiz == '3' :
+        break
+    else:
+        print(float("siz" ,2023-yoshingiz , "yilda tug'ulgansiz!"))
